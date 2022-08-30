0b4ce7bae6f070e034d0e2a751385abcc0b496e6
reddyalex <reddy@LECTURERL204>
2022-08-30 11:40:04 +0700

    Persiapan Class sudah siap

---
+++ Minggu2 - PhoneBook/Class/Contact.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minggu2___PhoneBook.Class
+{
+    class Contact
+    {
+
+        public String Name { get; set; }
+        public String Phone { get; set; }
+        private Boolean emergency;
+
+        public Boolean EmergencyContact
+        {
+            get { return emergency; }
+            set { emergency = value; }
+        }
+        public Contact()
+        {
+            Name = "";
+            Phone = "";
+            emergency = false;
+        }
+
+        public Contact(string name, string phone, bool emergencyContact)
+        {
+            Name = name;
+            Phone = phone;
+            EmergencyContact = emergencyContact;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + Environment.NewLine + 
+                   "Phone: " + Phone + Environment.NewLine + 
+                   "Emergency: " + emergency + Environment.NewLine;
+        }
+
+    }
+}
---
+++ Minggu2 - PhoneBook/Class/PhoneBook.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minggu2___PhoneBook.Class
+{
+    class PhoneBook
+    {
+        private List<Contact> contacts;
+        private int numOfContacts;
+
+        public PhoneBook(int numOfContacts)
+        {
+            this.numOfContacts = numOfContacts;
+            contacts = new List<Contact>();            
+        }
+
+        public bool addContact(Contact contact)
+        {
+            if (contacts.Count >= numOfContacts) return false;
+            contacts.Add(contact);
+            return true;
+        }
+        public int getSize()
+        {
+            return contacts.Count;
+        }
+
+        public List<Contact> viewAllContacts()
+        {
+            return contacts;
+        }
+
+        public List<Contact> viewEmergencyContacts()
+        {
+            //List<Contact> temp = new List<Contact>();
+            //foreach (var c in contacts)
+            //{
+            //    if (c.EmergencyContact == true)
+            //    {
+            //        temp.Add(c);
+            //    }
+            //}
+            //return temp;
+
+            return contacts.FindAll((x) => x.EmergencyContact == true);
+
+        }
+
+        public string searchContact(string name)
+        {
+            //contacts.IndexOf() // tidak cocok untuk kasus ini karena compare objek secara utuh bukan mencari bagian dari objek
+            var x = contacts.Find((c) => c.Name == name);
+            if (x is null) 
+                return "Contact not found";
+            else 
+                return x.Name + " " + x.Phone;                   
+        }
+
+        public void delete(string name)
+        {
+            //var x = contacts.Find((c) => c.Name == name);
+            //if (x != null)
+            //    contacts.Remove(x);
+
+            var jum = contacts.RemoveAll((x) => x.Name == name);
+            MessageBox.Show(jum + " data deleted");
+
+            
+        }
+
+        public void deleteAll()
+        {
+            contacts.Clear();
+        }
+
+        public bool isEmpty()
+        {
+            return contacts.Count == 0;
+        }
+        public bool isFull()
+        {
+            return contacts.Count == numOfContacts;
+        }
+    }
+}
